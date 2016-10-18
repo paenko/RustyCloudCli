3c4864809c3fdb26a036276bda5e7a72fdd3ff09
kper <kevp.per@gmail.com>
2016-10-18 07:40:58 +0100

    fix URL for DELETE

--- src/main.rs
+++ src/main.rs
@@ -49,101 +49,86 @@ struct DocFile {
     payload: String,
 }
 
-impl DocFile{
-    fn open(path : &Path) -> Self
-    {
-      let res : DocFile = decode_from(&mut File::open(&path).unwrap(), SizeLimit::Infinite).unwrap();
-      res
+impl DocFile {
+    fn open(path: &Path) -> Self {
+        let res: DocFile = decode_from(&mut File::open(&path).unwrap(), SizeLimit::Infinite)
+            .unwrap();
+        res
     }
 
-    fn create(path : &Path) -> Self
-    {
-      let mut fl : File = File::open(path).unwrap();
-      let mut buf = Vec::new();
-      fl.read_to_end(&mut buf);
-      DocFile
-      {
-        filename: path.to_str().unwrap().to_string(),
-        fileId: Uuid::new_v4(),
-        payload: base64::encode(&buf),
-      }
+    fn create(path: &Path) -> Self {
+        let mut fl: File = File::open(path).unwrap();
+        let mut buf = Vec::new();
+        fl.read_to_end(&mut buf);
+        DocFile {
+            filename: path.to_str().unwrap().to_string(),
+            fileId: Uuid::new_v4(),
+            payload: base64::encode(&buf),
+        }
     }
 
-    fn newSent(fln : String, fid: Uuid, py: Vec<u8>) -> Self
-    {
-      DocFile
-      {
-        filename: fln,
-        fileId: fid,
-        payload: base64::encode(&py),
-      }
+    fn newSent(fln: String, fid: Uuid, py: Vec<u8>) -> Self {
+        DocFile {
+            filename: fln,
+            fileId: fid,
+            payload: base64::encode(&py),
+        }
     }
 
     fn writeFile(&self) {
-      let mut f =
-            OpenOptions::new().write(true).create(true).open("Test").unwrap();
-      encode_into(self, &mut f, SizeLimit::Infinite);
+        let mut f = OpenOptions::new().write(true).create(true).open("Test").unwrap();
+        encode_into(self, &mut f, SizeLimit::Infinite);
     }
-
-
 }
 
 fn main() {
-    let args : Args = Docopt::new(USAGE)
-                      .and_then(|dopt| dopt.decode())
-                      .unwrap_or_else(|e| e.exit());
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|dopt| dopt.decode())
+        .unwrap_or_else(|e| e.exit());
     println!("{:?}", args);
 
     let mut SyncedFiles = HashMap::new();
     SyncedFiles.insert("1", "Data.txt");
 
-    if(args.cmd_sync)
-    {
-      sync(args);
+    if (args.cmd_sync) {
+        sync(args);
+    } else if (args.cmd_get) {
+        get(args);
+    } else if (args.cmd_delete) {
+        delete(args);
     }
-    else if(args.cmd_get)
-    {
-      get(args);
-    }
-    else if(args.cmd_delete)
-    {
-      delete(args);
-    }
-    let mut f =
-          OpenOptions::new().write(true).create(true).open("Synced.syn").unwrap();
+    let mut f = OpenOptions::new().write(true).create(true).open("Synced.syn").unwrap();
     encode_into(&SyncedFiles, &mut f, SizeLimit::Infinite);
 }
 
-fn sync(args: Args)
-{
-  let rp = &args.arg_path.unwrap();
-  let p = Path::new(&rp);
-  let object = DocFile::create(&p);
-  println!("{}", object.payload.len());
-  let res = RestClient::post("http://127.0.0.1:8080/file",
-                                    &json::encode(&object).unwrap(), 
-                                    "application/json").unwrap();
-  println!("{}", res);
-  //println!("{}", RestClient::post("https://jsonplaceholder.typicode.com/posts",
-  //                                  &json::encode(&object).unwrap(), 
-  //                                  "application/json").unwrap());
+fn sync(args: Args) {
+    let rp = &args.arg_path.unwrap();
+    let p = Path::new(&rp);
+    let object = DocFile::create(&p);
+    println!("{}", object.payload.len());
+    let res = RestClient::post("http://127.0.0.1:8080/file",
+                               &json::encode(&object).unwrap(),
+                               "application/json")
+        .unwrap();
+    println!("{}", res);
+    // println!("{}", RestClient::post("https://jsonplaceholder.typicode.com/posts",
+    //                                  &json::encode(&object).unwrap(),
+    //                                  "application/json").unwrap());
 }
 
-fn get(args: Args)
-{
+fn get(args: Args) {
     let sid = &args.arg_id.unwrap();
     let url = format!("http://127.0.0.1:8080/files/{}", sid);
     let st = RestClient::get(&url).unwrap().body;
-    //decode
+    // decode
     let mut f = OpenOptions::new().write(true).create(true).open("Test").unwrap();
     encode_into(&st, &mut f, SizeLimit::Infinite);
 }
 
-fn delete(args: Args)
-{
+fn delete(args: Args) {
     let sid = &args.arg_id.unwrap();
-    let url = format!("http://127.0.0.1:8080/file/{}", sid);
+    let url = format!("http://127.0.0.1:8080/files/{}", sid);
     println!("{}", RestClient::delete(&url).unwrap());
 }
 
-//File Handler
+// File Handler
